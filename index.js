c5105fb5080c3b465d10021914fac0f28f5a8b8c
paul kairu <paulkairukamau@gmail.com>
2024-10-13 20:52:27 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+// Set up a DOMContentLoaded event listener
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const paragraph = document.getElementById("text");
+
+    
+    paragraph.textContent = "This is really cool!";
+});
